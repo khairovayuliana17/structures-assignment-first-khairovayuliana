0ad648f9b6d0e4e1d5621692c3fbfca47347285f
khairovayuliana <y.khayrova@inbox.ru>
2025-06-05 17:52:39 +0300

    solution to task1

--- include/circle.h
+++ include/circle.h
@@ -1,14 +1,16 @@
 
 // declare Point structure here
- struct Point {
-      // your code here
- };
+struct Point {
+    // your code here
+    double x, y;
+};
 
 // declare Circle structure here
 struct Circle {
-  // your code here ("center" and "radius" (look for expected names in test.cpp))
-
+    // your code here ("center" and "radius" (look for expected names in test.cpp))
+    Point center;
+    double radius;
 };
 double circleArea(Circle c);
 double circleLength(Circle c);
-bool isPointInside(Circle c, Point p); 
+bool isPointInside(Circle c, Point p);
